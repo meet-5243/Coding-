74f066cd2cbe295bb6304f4f0e8fabc634439804
meet-5243 <meetpatel5243@gmail.com>
2025-07-29 09:36:46 +0530

    Time Based Key-Value Store

---
+++ 1695.cpp
@@ -0,0 +1,36 @@
+class TimeMap {
+public:
+    TimeMap() {
+        
+    }
+    
+    map<string,vector<pair<int,string>>>cnt;
+    void set(string key, string value, int timestamp) {
+        cnt[key].push_back({timestamp,value});
+    }
+    
+    string get(string key, int timestamp) {
+        auto &p=cnt[key];
+        int l=0,r=p.size()-1;
+        string s;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(p[mid].first<=timestamp){
+                s=p[mid].second;
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+    }
+
+    return s;
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
